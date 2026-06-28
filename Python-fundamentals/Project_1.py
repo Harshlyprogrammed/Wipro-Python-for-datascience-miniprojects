3f6ace1fce6161b86fb0cdcaf089fd9d61832d97
Harsh Sharma <harshsharmak007@gmail.com>
2026-06-28 11:26:03 +0530

    Create Project_1.py

---
+++ Python-fundamentals/Project_1.py
@@ -0,0 +1,8 @@
+distance = int(input("How far you want to go : "))
+
+if distance<3 :
+    print("Ride a Bicycle")
+elif 3<=distance<300:
+    print("Ride a motorcycle")
+else:
+    print("Ride a super-car")

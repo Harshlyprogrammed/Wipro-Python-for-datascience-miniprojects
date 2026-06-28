490457ce69b772238a1553de587c62a8828b4745
Harsh Sharma <harshsharmak007@gmail.com>
2026-06-28 20:52:21 +0530

    assignment folder is added

---
+++ Python-fundamentals/Assignment/assig_1.py
@@ -0,0 +1,9 @@
+#Write a program to check if a given number is Positive, Negative, or Zero.
+num = int(input("write the number : "))
+
+if num>0:
+    print("the number is positive")
+elif num<0:
+    print("the number is negative")
+else :
+    print("the number is zero")
---
+++ Python-fundamentals/Assignment/assig_2.py
@@ -0,0 +1,9 @@
+#Write a program to check if a given number is odd or even.
+
+num = int(input("write the number : "))
+
+if num%2==0:
+    print("the number is even")
+
+else :
+    print("the number is odd")
